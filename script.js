a7a9c894dc46e2dae6d7fcec694308c336ce351a
Jake Wagner <jkwagneriii@gmail.com>
2020-10-07 09:06:33 -0500

    Put down opening psuedo code as well as started working on the layout through HTML. added very simple nav bar as a starting place.

---
+++ script.js
@@ -0,0 +1,13 @@
+//First I need to create the divs for each section of weather information. There will be a header at the top of the page that doesn't need any sort of interaction. My first goal will be to get a single location to show the current weather. Then I'll move on to having the 5-Day forcast display. THEN I'll work on the city search bar and how that interacts with the weather being displayed. 
+
+
+
+
+
+
+//By city name
+// You can search weather forecast for 5 days with data every 3 hours by city name. All weather data can be obtained in JSON and XML formats.
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+// Call current UV data By geographic coordinates
+// ("http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}")
